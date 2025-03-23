74cd76e2002c9f2fedaf5affb8fbbc07222742ed
Suprio-Das <priyamd507@gmail.com>
2025-03-23 13:37:34 +0600

    Basic server setup completed and server is running on PORT

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello, Express Server basic installation completed!!!');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Root Express Route
+app.get('/', (req, res) => {
+    res.send('Hola, Job Porter Server is Boiling!!!');
+})
+
+app.listen(PORT);
