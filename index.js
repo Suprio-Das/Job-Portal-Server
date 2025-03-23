47993e35cff741e01b0aa1efd891e85a91cb25e0
Suprio-Das <priyamd507@gmail.com>
2025-03-23 13:47:23 +0600

    MongoDB connected successfully

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 // Middlewares
 app.use(express.json());
@@ -11,5 +13,27 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hola, Job Porter Server is Boiling!!!');
 })
+
+// Connect MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63zdo.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.listen(PORT);
